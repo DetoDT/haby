a1c6f8988b15285373574107df62aeb30fef0116
Francesco <francescodeto@gmail.com>
2024-09-26 14:23:20 +0200

    Added reading and writing on yaml file to store tasks and their status

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import yaml
+
+
+with open('tasks.yml', 'r') as file:
+    tasks = yaml.safe_load(file)
+
+
 # Main object of the project is the Task: it contains
 # the name, the points and the daily status of the class
 class Task():
@@ -8,6 +15,7 @@ class Task():
 
     def setCompleted(self):
         self.completed = True
+
     def setNotCompleted(self):
         self.completed = False
 
@@ -26,12 +34,13 @@ class Task():
     def getName(self):
         return self.name
 
-# TODO: daily score update 
-def changeScore(task):
-    if task.completed == True:
-        score += task.points
-    else:
-        score -= task.points
+# # TODO: daily score update
+# def changeScore(task):
+#     if task.completed == True:
+#         score += task.points
+#     else:
+#         score -= task.points
+
 
 # create a task and assign name and points
 def createTask():
@@ -39,7 +48,24 @@ def createTask():
     taskPoints = int(input("Points: "))
     return Task(taskName, taskPoints, False)
 
-# manages listing, adding, setting status of tasks and quitting out
+
+def save(taskList):
+    print("Saving...")
+    i = 0
+    dict = {}
+    for task in taskList:
+        dict[f'Task{i}'] = {'name': task.getName(
+        ), 'points': task.getPoints(), 'completed': task.getCompleted()}
+        i += 1
+
+    with open('tasks.yml', 'w') as file:
+        yaml.dump(dict, file)
+
+    file.close()
+
+    print("Saved!")
+
+
 def performAction(action, taskList, score):
     match action:
         case 'l':
@@ -48,7 +74,8 @@ def performAction(action, taskList, score):
                 status = "Not Completed"
                 if i.getCompleted():
                     status = "Completed"
-                print(f'- {i.getName()}, value: {i.getPoints()}, status: {status}')
+                print(
+                    f'- {i.getName()}, value: {i.getPoints()}, status: {status}')
             print("\n")
             return False, score
 
@@ -65,16 +92,17 @@ def performAction(action, taskList, score):
             for i in range(1, len(taskList)+1):
                 print(f'{i}. {taskList[i-1].getName()}')
             index = int(input("\nEnter number: ")) - 1
-            if taskList[index].getCompleted() == True:
+            if taskList[index].getCompleted():
                 taskList[index].setNotCompleted()
                 score -= taskList[index].getPoints()
             else:
-                taskList[index].setCompleted() 
+                taskList[index].setCompleted()
                 score += taskList[index].getPoints()
 
             print('\n')
             return False, score
         case 'q':
+            save(taskList)
             return True, score
 
 # TODO: save config file
@@ -82,16 +110,25 @@ def performAction(action, taskList, score):
 # TODO: gui
 # TODO: plot graph
 
+
 def main():
     score = 0
-    exampleTask = Task("Coding", 100, False)
+    # exampleTask = Task("Coding", 100, False)
     actionList1 = ['l', 'a', 's', 'q']
-    taskList = [exampleTask]
-    name = exampleTask.getName()
-    print(f'{name}')
+    taskList = []
+
+    for k in tasks:
+        # task = Task(tasks[k][0], tasks[k][1], tasks[k][2])
+        attributes = tasks[k]
+        loadedTask = Task(
+            attributes['name'], attributes['points'], attributes['completed'])
+        taskList.append(loadedTask)
+    file.close()
+
     print("Haby.\n")
+
     while True:
-        print ('----------------------------------------------------------------')
+        print('--------------------------------------------------------------')
         print(f'Score: {score}')
         print("l: list tasks | a: add task | s: set completed | q: quit")
         action = input()
@@ -102,8 +139,6 @@ def main():
         else:
             print("Please try again.")
 
-
-    
 
 if __name__ == '__main__':
     main()
